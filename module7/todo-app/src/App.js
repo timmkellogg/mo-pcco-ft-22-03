9546efcf9ba6d035945a1bf95a6612390ca7c805
Tim Kellogg <timmkellogg@gmail.com>
2022-06-06 19:01:31 -0400

    module 7 session 1 content

---
+++ module7/todo-app/src/App.js
@@ -0,0 +1,11 @@
+import TodoClass from './ToDoClass';
+
+function App() {
+  return (
+    <div>
+      <TodoClass />
+    </div>
+  );
+}
+
+export default App;
---
+++ module7/todo-app/src/ToDoClass.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class ToDoClass extends React.Component {    
+
+    state = {
+        toDoInput: '',
+        toDoList: ['teach javascript']
+    }
+
+    addToDo = () => {
+        if (!this.state.toDoInput.trim()) return;
+
+        const newList = [...this.state.toDoList];
+
+        newList.push(this.state.toDoInput);
+
+        this.setState({ toDoList: newList, toDoInput: '' });
+    }
+
+    handleChangeInput = (event) => {
+        this.setState({ toDoInput: event.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>To Do App</h2>
+                <input value={this.state.toDoInput} onChange={this.handleChangeInput} />
+                <button onClick={this.addToDo}>add todo</button>
+                <ul>
+                    {this.state.toDoList.map((item, key) => {
+                        return <li key={key}>{item}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ToDoClass;
